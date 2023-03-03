a1c13fbaad7900cb5355b32b54ef9842da6e212d
Vsevolod <Sev7715@gmail.com>
2023-03-03 18:23:49 +0300

    Оптимизировал код с помощью классов и конструкторов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,13 +6,11 @@ public class MyFirstProgram {
    hello("user");
    hello("Alexei");
 
-   double l = 5;
-   System.out.println("Площадь квадрата со сторой " + l + " = " + area(l));
+   Square s = new Square(5);
+   System.out.println("Площадь квадрата со сторой " + s.l + " = " + area(s));
 
-   double a = 4;
-   double b = 6;
-
-   System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+   Rectangle r = new Rectangle(4, 6);
+   System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 
   }
 
@@ -20,11 +18,11 @@ public class MyFirstProgram {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area (double len) {
-    return  len * len;
+  public static double area (Square s) {
+    return  s.l * s.l;
   }
 
-  public static double area (double a, double b) {
-    return  a*b;
+  public static double area (Rectangle r) {
+    return  r.a * r.b;
   }
 }
