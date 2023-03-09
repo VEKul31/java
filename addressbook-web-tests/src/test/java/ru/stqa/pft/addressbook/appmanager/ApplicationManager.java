5229be68284fc52501283d21685e487c55fffcb3
Vsevolod <Sev7715@gmail.com>
2023-03-09 15:23:00 +0300

    Создал общий базовый класс для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,12 +1,8 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.concurrent.TimeUnit;
 
@@ -32,24 +28,6 @@ public class ApplicationManager {
 
 	public void stop() {
 		wd.quit();
-	}
-
-	public boolean isElementPresent(By by) {
-		try {
-			wd.findElement(by);
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
-
-	public boolean isAlertPresent() {
-		try {
-			wd.switchTo().alert();
-			return true;
-		} catch (NoAlertPresentException e) {
-			return false;
-		}
 	}
 
 	public GroupHelper getGroupHelper() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,36 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactHelper {
-	private WebDriver wd;
+public class ContactHelper extends HelperBase {
 
 	public ContactHelper(WebDriver wd) {
-		this.wd = wd;
+		super(wd);
 	}
 
 	public void returnToHomePage() {
-		wd.findElement(By.linkText("home page")).click();
+		click(By.linkText("home page"));
 	}
 
 	public void enterContactCreation() {
-		wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+		click(By.xpath("//div[@id='content']/form/input[21]"));
 	}
 
 	public void fillContactForm(ContactData contactData) {
-		wd.findElement(By.name("firstname")).click();
-		wd.findElement(By.name("firstname")).clear();
-		wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
-		wd.findElement(By.name("theform")).click();
-		wd.findElement(By.name("middlename")).click();
-		wd.findElement(By.name("lastname")).click();
-		wd.findElement(By.name("lastname")).clear();
-		wd.findElement(By.name("lastname")).sendKeys(contactData.getLast_name());
-		wd.findElement(By.name("mobile")).click();
-		wd.findElement(By.name("mobile")).clear();
-		wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-		wd.findElement(By.name("email")).click();
-		wd.findElement(By.name("email")).clear();
-		wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+		type(By.name("firstname"), contactData.getName());
+		type(By.name("lastname"), contactData.getLast_name());
+		type(By.name("mobile"), contactData.getMobile());
+		type(By.name("email"), contactData.getEmail());
 	}
 
 }
