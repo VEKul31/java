00e99031498a31da21b1fb45c17e574c881f3eaa
Vsevolod <Sev7715@gmail.com>
2023-03-10 15:18:02 +0300

    Доработал запуск тестов разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -23,11 +23,11 @@ public class ApplicationManager {
 	}
 
 	public void init() {
-		if (browser == BrowserType.FIREFOX) {
+		if (browser.equals(BrowserType.FIREFOX)) {
 			wd = new FirefoxDriver();
-		} else if (browser == BrowserType.CHROME) {
+		} else if (browser.equals(BrowserType.CHROME)) {
 			wd = new ChromeDriver();
-		} else if (browser == BrowserType.EDGE){
+		} else if (browser.equals(BrowserType.EDGE)){
 			System.setProperty("webdriver.edge.driver", "C:\\Webdriver\\msedgedriver.exe");
 			wd = new EdgeDriver();
 		}
