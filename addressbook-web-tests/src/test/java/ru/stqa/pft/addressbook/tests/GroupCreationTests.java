4b0740abd7f89c9cdb25ad27b6b9502fe4c6eeb2
Vsevolod <Sev7715@gmail.com>
2023-03-15 10:57:05 +0300

    перестроил с помощью Fluent-интерфейса структуру для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,13 +13,13 @@ public class GroupCreationTests extends TestBase {
 	public void testGroupCreation() {
 		app.goTo().groupPage();
 		List<GroupData> before = app.group().list();
-		GroupData group = new GroupData("test1", null, null);
+		GroupData group = new GroupData().withName("test1");
 		app.group().create(group);
 		List<GroupData> after = app.group().list();
 		Assert.assertEquals(after.size(), before.size() + 1);
 
 		before.add(group);
-		Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+		Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
 		before.sort(byId);
 		after.sort(byId);
 
