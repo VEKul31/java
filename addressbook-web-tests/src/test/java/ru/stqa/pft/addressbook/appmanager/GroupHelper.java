fe8e95f1ef38801bcb7ff8f51e13f0a9dfd13868
Vsevolod <Sev7715@gmail.com>
2023-03-28 18:34:14 +0300

    Убрал неиспользуемые методы, почистил импорты для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -6,10 +6,7 @@ import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -37,10 +34,6 @@ public class GroupHelper extends HelperBase {
 
 	public void deleteSelectedGroups() {
 		click(By.name("delete"));
-	}
-
-	public void selectGroup(int index) {
-		wd.findElements(By.name("selected[]")).get(index).click();
 	}
 
 	public void selectGroupById(int id) {
@@ -71,12 +64,6 @@ public class GroupHelper extends HelperBase {
 		returnToGroupPage();
 	}
 
-	public void delete(int index) {
-		selectGroup(index);
-		deleteSelectedGroups();
-		returnToGroupPage();
-	}
-
 	public void delete(GroupData group) {
 		selectGroupById(group.getId());
 		deleteSelectedGroups();
@@ -92,16 +79,7 @@ public class GroupHelper extends HelperBase {
 		return wd.findElements(By.name("selected[]")).size();
 	}
 
-	public List<GroupData> list() {
-		List<GroupData> groups = new ArrayList<GroupData>();
-		List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
-		for (WebElement element : elements) {
-			String name = element.getText();
-			int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-			groups.add(new GroupData().withId(id).withName(name));
-		}
-		return groups;
-	}
+
 
 	private Groups groupCache = null;
 
