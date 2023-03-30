49dafd439bb3bad4c4ff2f93797710cc450493d3
Vsevolod <Sev7715@gmail.com>
2023-03-30 09:12:44 +0300

    Лекция 5.9. До добавления атрибутов в ContactData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -25,7 +25,7 @@ public class ContactHelper extends HelperBase {
 		click(By.xpath("//div[@id='content']/form/input[21]"));
 	}
 
-	public void fillContactForm(ContactData contactData, boolean creation ) {
+	public void fillContactForm(ContactData contactData, boolean creation) {
 		type(By.name("firstname"), contactData.getName());
 		type(By.name("lastname"), contactData.getLastName());
 		type(By.name("mobile"), contactData.getMobile());
@@ -112,5 +112,29 @@ public class ContactHelper extends HelperBase {
 			click(By.linkText("home"));
 		}
 
+	}
+
+	public ContactData infoFromEditForm(ContactData contact) {
+		initContactModificationById(contact.getId());
+		String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
+		String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
+		String home = wd.findElement(By.name("home")).getAttribute("value");
+		String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
+		String work = wd.findElement(By.name("work")).getAttribute("value");
+		wd.navigate().back();
+		return new ContactData().withId(contact.getId()).withName(firstname).withLastName(lastname)
+					.withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work);
+	}
+
+	public void initContactModificationById(int id) {
+		WebElement checkbox = wd.findElement(By.cssSelector(String.format("input[value='%s']", id)));
+		WebElement row = checkbox.findElement(By.xpath("./../.."));
+		List<WebElement> cells = row.findElements(By.tagName("td"));
+		cells.get(7).findElement(By.tagName("a")).click();
+
+		//or
+		//wd.findElement(By.xpath(String.format("//input[@value='%s']/../../td[8]/a", id)).click();
+		//wd.findElement(By.xpath(String.format("//tr[.//input[@value='%s']]/td[8]/a", id)).click();
+		//wd.findElement(By.cssSelector(String.format("a[href='edit.php?id=%s']", id)).click();
 	}
 }
