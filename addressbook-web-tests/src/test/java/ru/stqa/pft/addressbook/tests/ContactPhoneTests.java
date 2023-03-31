c160fe28a471b6c333fc30e9250a74f98f1b32f3
Vsevolod <Sev7715@gmail.com>
2023-03-31 13:55:00 +0300

    Добавил предусловие в тест проверки телефона контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -12,6 +11,16 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactPhoneTests extends TestBase {
+
+	@BeforeMethod
+	public void ensurePreconditions() {
+		app.goTo().homePage();
+		if (app.contact().all().size() == 0) {
+			app.goTo().contactPage();
+			app.contact().create(new ContactData().withName("name").withLastName("last").withHomePhone("111").withMobilePhone("222").withWorkPhone("333")
+						.withEmail("test@google.com").withGroup(null));
+		}
+	}
 
 	@Test
 	public void testContactPhones() {
