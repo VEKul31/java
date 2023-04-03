f0365ca348c33551fb165ca08dbe3ea8dcad7fc9
Vsevolod <Sev7715@gmail.com>
2023-04-03 17:57:49 +0300

    Применил параметризацию для тестов групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,23 +1,36 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-	@Test
-	public void testContactCreationTests() {
+	@DataProvider
+	public Iterator<Object[]> validContacts() {
+		File photo = new File("src/test/resources/stru.png");
+		List<Object[]> list = new ArrayList<Object[]>();
+		list.add(new Object[] {new ContactData().withName("name").withLastName("last").withPhoto(photo).withMobilePhone("123").withEmail("test@google.com").withGroup("test1")});
+		list.add(new Object[] {new ContactData().withName("name2").withLastName("last2").withPhoto(photo).withMobilePhone("1234").withEmail("tes@google.com").withGroup("test2")});
+		list.add(new Object[] {new ContactData().withName("name3").withLastName("last3").withPhoto(photo).withMobilePhone("12345").withEmail("te@google.com").withGroup("test3")});
+		return list.iterator();
+	}
+
+	@Test(dataProvider = "validContacts")
+	public void testContactCreationTests(ContactData contact) {
 		app.goTo().homePage();
 		Contacts before = app.contact().all();
 		app.goTo().contactPage();
-		File photo = new File("src/test/resources/stru.png");
-		ContactData contact = new ContactData().withName("name").withLastName("last").withPhoto(photo).withMobilePhone("1234567").withEmail("test@google.com").withGroup("test1");
 		app.contact().create(contact);
 		assertThat(app.contact().getContactCount(),equalTo(before.size() +1));
 		Contacts after = app.contact().all();
